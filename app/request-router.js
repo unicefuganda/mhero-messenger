6d1decab926762b45a5da74d6c4a6621cee7180b
Andrew Mukiza <andrewmukiza@gmail.com>
2014-09-09 17:15:24 +0300

    handle uploads

--- app/request-router.js
+++ app/request-router.js
@@ -1,7 +1,8 @@
-var finalHandler = require('finalhandler');
-var http = require('http');
-var serveStatic = require('serve-static');
-var serve = serveStatic(__dirname + '/../static', {'index': ['landing_page.html', 'index.html']});
+var finalHandler = require('finalhandler'),http = require('http'),
+serveStatic = require('serve-static'),
+serve = serveStatic(__dirname + '/../static', {'index': ['landing_page.html', 'index.html']}),
+fs = require('fs'),
+path = require('path');
 
 var RequestRouter = function(){
 	var handleGet = function(req, res){
@@ -10,7 +11,25 @@ var RequestRouter = function(){
 	};
 
 	var handlePost = function(req, res){
-		console.log(req.method)
+		var formidable = require('formidable'),
+			util = require('util'),
+			form = new formidable.IncomingForm();
+			form.uploadDir  = __dirname + '/../uploads/'
+
+    form.parse(req, function(err, fields, files) {
+      var oldPath = files.file.path,
+      dir = path.dirname(oldPath)
+      fs.rename(oldPath, path.join(dir, files.file.name),  function(err){
+      	console.log(err)
+      });
+
+      res.writeHead(302, {
+        	'content-type': 'text/html',
+        	'Location': '/'
+      });
+
+      res.end();
+    });
 	};
 
 	var handleProhibitted = function(req, res){
