4a7dd7a9887a2cdff70020ba08cc4148c9c0fed8
Jeff Xiong <gigix1980@gmail.com>
2014-09-09 14:27:22 +0300

    move server.js to scripts

--- scripts/server.js
+++ scripts/server.js
@@ -1,8 +1,12 @@
+#!/usr/bin/env node
+
 var finalHandler = require('finalhandler');
 var http = require('http');
 var serveStatic = require('serve-static');
 
-var serve = serveStatic('static', {'index': ['landing_page.html', 'index.html']});
+var rootDir = __dirname + '/..';
+
+var serve = serveStatic(rootDir + '/static', {'index': ['landing_page.html', 'index.html']});
 
 var server = http.createServer(function(req, res){
     var done = finalHandler(res, res);
