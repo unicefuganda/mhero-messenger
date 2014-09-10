f748096b8e914f4890e096cd8169b136b7f79ffd
Jeff Xiong <gigix1980@gmail.com>
2014-09-10 16:25:57 +0300

    add a log on server

--- scripts/server.js
+++ scripts/server.js
@@ -6,6 +6,8 @@ var RequestRouter = require(__dirname + '/../app/request-router');
 var router = new RequestRouter();
 
 var server = http.createServer(function(req, res){
+    console.log(new Date());
+    console.log('<-- ' + req.method + ' ' + req.url);
     router.handle(req, res);
 });
 
